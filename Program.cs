ffad5757cf85caf2063c0d8e44aaade570971117
Tom Dugan <tdugan85@gmail.com>
2018-09-22 12:21:48 -0500

    completed exercises 1 and 2

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Restriction / Filtering Operations
+            var restrictionOperations = new RestrictionOperations();
+
+            Console.WriteLine("Exercise One:");
+            restrictionOperations.ExerciseOne();
+            Console.WriteLine("");
+
+            Console.WriteLine("Exercise Two:");
+            restrictionOperations.ExerciseTwo();
+            Console.WriteLine("");
+
+            // Ordering Operations
+
+            var orderingOperations = new OrderingOps();
+
+            Console.WriteLine("Exercise Three:");
+            orderingOperations.ExerciseThree();
+            Console.WriteLine("");
+
+            Console.WriteLine("Exercise Four:");
+            orderingOperations.ExerciseFour();
+            Console.WriteLine("");
+
+            // Aggregate Operations
+            var aggregateOperations = new AggregateOperations();
+
+            Console.WriteLine("Exercise Five:");
+            aggregateOperations.ExerciseFive();
+            Console.WriteLine("");
+
+            // Partitioning Operations
+
+            // Using Custom Types
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RestrictionOperations.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQedList
+{
+    class RestrictionOperations
+    {
+        public void ExerciseOne()
+        {
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+             IEnumerable<string> LFruits =
+                from fruit in fruits
+                where fruit.StartsWith("L")
+                select fruit;
+
+            foreach(var fruit in LFruits)
+            {
+                if (fruit.StartsWith("L")) 
+                {
+                    Console.WriteLine(fruit);
+                }
+            }
+
+            // Console.Write(LFruits);
+        }
+
+        public void ExerciseTwo()
+        {
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            IEnumerable<int> fourSixMultiples = numbers.Where(num => (num % 4) == 0 || (num % 6) == 0);
+
+            foreach (int num in fourSixMultiples)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
